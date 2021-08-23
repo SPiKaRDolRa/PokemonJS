da5bd8cc41eff91434dd1b08ba1be596f8a58916
SPiKaRDolRa <become_aum@hotmail.com>
2021-08-23 16:02:45 +0700

    base-on

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Form from './Form'
+import Pokemon from './Pokemon'
+import Stock from './Stock'
+import Datetime from './Datetime'
+import Array from './Array'
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <Navbar />
+                <Switch>
+                <Route path="/" exact>
+                    <Form />
+                </Route>
+                <Route path="/pokemon" exact>
+                    <Pokemon />
+                </Route>
+                <Route path="/stock" exact>
+                    <Stock />
+                </Route>
+                <Route path="/datatime" exact>
+                    <Datetime />
+                </Route>
+                <Route path="/array" exact>
+                    <Array />
+                </Route>
+                </Switch> 
+        </Router>
     )
 }
 
---
+++ src/Datetime.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Datetime = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Datetime
