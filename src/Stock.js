a5543e9a51f68cb8ac53b1bba240408c499cace6
SPiKaRDolRa <become_aum@hotmail.com>
2021-08-25 17:58:05 +0700

    Commit-stock

--- src/Stock.js
+++ src/Stock.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import JsonData from './exam-data.json'
-
-console.log(JsonData[20].products[0].id)
 
 const Stock = () => {
 
@@ -9,18 +7,22 @@ const Stock = () => {
         <div className="section">
             <div className="container">
                 <div className="content">
-                    <p className="titleform">Fetch product on json file</p>
-                    <p className="mt-4">Product name was editabled price</p>
-                    {JsonData.filter( json1 => json1.is_editable_price === false)
-                             .map( json1 => { 
-                                return <p className="titlexp">- {json1.name}</p> })
-                    }
-                    <p className="mt-4">SUM of products weight</p>
-                    {JsonData.filter( json2 => json2.is_editable_price === false)
-                             .map(json2 => json2.products.map(products => {
-                                return <p>{products.weight}</p>
-                             }))
-                    }
+                    <div className="text-center">
+                        <p className="titleform">Fetch product on json file</p>
+                        <p className="mt-4">Products name was editabled price</p>
+                        {JsonData.filter( json1 => json1.is_editable_price === false)
+                                    .map( json1 => { 
+                                    return <p className="titlexp"> {json1.name}</p> })
+                        }
+                        <p className="mt-4">Total weight of products was editabled price</p>
+                        {JsonData.filter( json2 => json2.is_editable_price === false)
+                                    .map( json2 => json2.Total = json2.products
+                                    .reduce((sum, cur) => sum + cur.weight, 0)) 
+                                        .reduce( (total, get) => {
+                                            return <p className="titlexp"> {total + get}</p>
+                                        })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
